4183cfbbac88f8e4fd3c2fd8abba3b8e07084d51
sedx876 <swatkins876@gmail.com>
2020-10-06 11:27:27 -0400

    work on header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './styles/App.css';
+import Home from './components/Home'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="app">
-      
+    <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../styles/Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <img
+        className="header__icon"
+        src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+        alt=""
+      /> 
+      <div className='header__center'>
+        <input type="text" />
+        {/* <SearchIcon /> */}
+      </div>
+      <div className='header__right'>
+        <p>Become a host</p>
+        {/* <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar /> */}
+      </div>
+    </div>
+  )
+}
+
+export default Header
