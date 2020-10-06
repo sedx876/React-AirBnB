91b561e9b9cbdd17aebc2f0166ef382dd235300b
sedx876 <swatkins876@gmail.com>
2020-10-06 11:45:41 -0400

    added material ui and react-router denpendencies

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './styles/App.css';
 import Home from './components/Home'
 import Header from './components/Header'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-    <Header />
+    <Router>
+      <Header />
       <Home />
+    </Router>
     </div>
   );
 }
