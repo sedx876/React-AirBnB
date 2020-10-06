8f2496341b83e71fb100d6cb576c91789befdc9d
sedx876 <swatkins876@gmail.com>
2020-10-06 13:19:04 -0400

    work on banner component

---
+++ src/components/Banner.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import '../styles/Banner.css'
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+
+function Banner() {
+
+  return (
+    <div className='banner'>
+      <div className='banner__search'>
+      <Button 
+      className='banner__searchButton' 
+      variant='outlined'>
+      Search
+      </Button>
+      </div>
+      <div className='banner__info'>
+        <h1>Get out and stretch your imagination</h1>
+        <h5>
+        Plan a different kind of getaway to uncover the hidden gems near you.
+        </h5>
+        <Button variant='outlined'>Explore Nearby</Button>
+      </div>
+    </div>
+  )
+
+}
+
+export default Banner
