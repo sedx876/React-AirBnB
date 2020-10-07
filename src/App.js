a86242e34261b59400872eee00df815ad6043280
sedx876 <swatkins876@gmail.com>
2020-10-07 07:51:44 -0400

    added footer component and styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles/App.css';
 import Home from './components/Home'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Header />
       <Home />
+      <Footer />
     </Router>
     </div>
   );
